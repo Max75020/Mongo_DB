9136b4be17051e6af6122620d081ac0d783a88ea
Max75020 <maxime.duplaissy@gmail.com>
2024-04-19 11:09:48 +0200

    Account  : ajouter,supprimer et mise à jour ok

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,20 +1,93 @@
+const Account = require("../models/account");
 // Récupération de tous les accounts
 exports.readAllAccounts = (req, res) => {
-	res.send("Read All Accounts");
+	try{
+		const accounts = Account.find({userId: req.auth.userId});
+		res.status(200).json(accounts);
+	}
+	catch(error){
+		res.status(500).json({
+			message: error.message || "Erreur lors de la récupération des comptes.",
+		});
+	}
 }
 // Récupération d'un account
-exports.readAccount = (req, res) => {
-	res.send("Read Account");
+exports.readAccount = async (req, res) => {
+	console.log(req.params.id);
+	try{
+		const account = await Account.findOne({ _id: req.params.id, userId: req.auth.userId });
+		if (!account) {
+			return res.status(404).json({
+				status: "fail",
+				message: "Pas de compte trouvé avec cet ID",
+			});
+		}
+		res.status(200).json(account);
+	}
+	catch(error){
+		res.status(500).json({
+			message: error.message || "Erreur lors de la récupération du compte.",
+		});
+	}
 }
-// création d'un account
-exports.createAccount = (req, res) => {
-	res.send("Create Account");
+// Création d'un account
+exports.createAccount = async (req, res) => {
+	try {
+		const { bankName, customName } = req.body;
+		const account = new Account({
+			bankName: bankName,
+			customName: customName,
+			userId: req.auth.userId,
+		});
+		await account.save();
+		res.status(201).json(account);
+	} catch (error) {
+		res.status(500).json({
+			message: error.message || " Erreur lors de la création du compte.",
+		});
+	}
 }
 // Mettre à jour un account
-exports.updateAccount = (req, res) => {	
-	res.send("Update Account");
-}
+exports.updateAccount = async (req, res) => {
+	try {
+		const { bankName, customName } = req.body;
+		const account = await Account.findByIdAndUpdate(
+			{ _id: req.params.id, userId: req.auth.userId },
+			{ bankName, customName },
+			{ new: true, runValidators: true }
+		);
+
+		if (!account) {
+			return res.status(404).json({
+				status: "fail",
+				message: "Pas de compte trouvé avec cet ID",
+			});
+		}
+
+		res.status(200).json(account);
+	} catch (error) {
+		res.status(500).json({
+			status: "error",
+			message: error,
+		});
+	}
+};
+
 // Supprimer un account
-exports.deleteAccount = (req, res) => {
-	res.send("Delete Account");
-}
+exports.deleteAccount = async (req, res) => {
+	try {
+		const account = await Account.findOneAndDelete({ _id: req.params.id, userId: req.auth.userId });
+		if (!account) {
+			return res.status(404).json({
+				status: "fail",
+				message: "Pas de compte trouvé avec cet ID",
+			});
+		}
+		res.status(200).json({message: "Compte supprimé avec succès"});
+	} catch (error) {
+		res.status(400).json({
+			status: "error",
+			message: error,
+		});
+	}
+};
--- app/models/account.js
+++ app/models/account.js
@@ -10,7 +10,7 @@ const accountSchema = new mongoose.Schema({
 		required: [true, "Le nom du compte est obligatoire"],
 		maxlength: [50, "Le nom du compte doit contenir au maximum 50 caractères"],
 	},
-	user: {
+	userId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "User",
 		required: [true, "L'utilisateur est obligatoire"],
@@ -19,6 +19,22 @@ const accountSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now
 	}
+});
+
+accountSchema.pre("save", function (next) {
+	this.lastUpdated = Date.now();
+	next();
+});
+
+accountSchema.pre("findOneAndUpdate", function (next) {
+	this.set({ lastUpdated: new Date.now() });
+	next();
+});
+
+accountSchema.pre("findOneAndDelete", async function (next) {
+	const accountId = this.getQuery()._id;
+	await mongoose.model("AccountLine").deleteMany({ account: accountId });
+	next();
 });
 
 const Account = mongoose.model("Account", accountSchema);
